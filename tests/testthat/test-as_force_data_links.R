b89a5416d6807e72f974b28862666f62b356032f
CJ Yetman <cj@2degrees-investing.org>
2020-12-18 15:35:13 +0100

    more precise testing of the results

--- tests/testthat/test-as_force_data_links.R
+++ tests/testthat/test-as_force_data_links.R
@@ -17,26 +17,30 @@ test_that("as_force_data_links() handles a data frame", {
   example <- data.frame(a = 0:1, b = 2:3)
   result <- as_force_data_links(example)
   test_deafult_characteristics(result)
-  expect_true(result[[1]] == 0:1 && result[[2]] == 2:3)
+  expect_identical(result[[1L]], c("0", "1"))
+  expect_identical(result[[2L]], c("2", "3"))
 })
 
 test_that("as_force_data_links() handles a list", {
   example <- list(list(source = 0, target = 2), list(source = 1, target = 3))
   result <- as_force_data_links(example)
   test_deafult_characteristics(result)
-  expect_true(result[[1]] == 0:1 && result[[2]] == 2:3)
+  expect_identical(result[[1L]], c("0", "1"))
+  expect_identical(result[[2L]], c("2", "3"))
 })
 
 test_that("as_force_data_links() handles a data frame with columns 'from' and 'to'", {
   example <- data.frame(from = 0:1, to = 2:3)
   result <- as_force_data_links(example)
   test_deafult_characteristics(result)
-  expect_true(result[[1]] == 0:1 && result[[2]] == 2:3)
+  expect_identical(result[[1L]], c("0", "1"))
+  expect_identical(result[[2L]], c("2", "3"))
 })
 
 test_that("as_force_data_links() handles a data frame with properly named columns in a different order", {
   example <- data.frame(target = 2:3, source = 0:1)
   result <- as_force_data_links(example)
   test_deafult_characteristics(result)
-  expect_true(result[[1]] == 0:1 && result[[2]] == 2:3)
+  expect_identical(result[[1L]], c("0", "1"))
+  expect_identical(result[[2L]], c("2", "3"))
 })
