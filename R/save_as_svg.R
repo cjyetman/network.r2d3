78d3dd08bedb058304f8d6c2e5e6525f4e1d532f
CJ Yetman <cj@cjyetman.com>
2021-03-24 09:27:20 +0100

    add rough save_as_svg function

---
+++ R/save_as_svg.R
@@ -0,0 +1,22 @@
+save_as_svg <-
+  function(widget, filepath, background = "white", delay = 0.5) {
+    tmp_html <- tempfile(fileext = ".html")
+    on.exit(unlink(tmp_html))
+    r2d3::save_d3_html(widget, file = tmp_html, background = background)
+
+    b <- chromote::ChromoteSession$new()
+    b$Page$navigate(paste0("file://", normalizePath(tmp_html, winslash = "/")))
+    Sys.sleep(delay)
+
+    eval <-
+      paste0(
+        'var svg_node = document.querySelector("div#htmlwidget_container :first-child").shadowRoot.firstChild;\n',
+        'new XMLSerializer().serializeToString(svg_node);'
+      )
+    svg <- b$Runtime$evaluate(eval)$result$value
+
+    b$close()
+
+    writeLines(svg, filepath)
+    invisible(filepath)
+  }
